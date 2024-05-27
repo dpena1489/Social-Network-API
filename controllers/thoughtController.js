a0fc56e328cb08f7b5ddb60163bab566c674d95f
dpena1489 <150938385+dpena1489@users.noreply.github.com>
2024-05-27 14:34:21 -0500

    approved commit

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -39,37 +39,4 @@ module.exports = {
       return res.status(500).json(err);
     }
   },
-  // Delete a course
-//   async deleteCourse(req, res) {
-//     try {
-//       const course = await Course.findOneAndDelete({ _id: req.params.courseId });
 
-//       if (!course) {
-//         res.status(404).json({ message: 'No course with that ID' });
-//       }
-
-//       await Student.deleteMany({ _id: { $in: course.students } });
-//       res.json({ message: 'Course and students deleted!' });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   },
-  // Update a course
-//   async updateCourse(req, res) {
-//     try {
-//       const course = await Course.findOneAndUpdate(
-//         { _id: req.params.courseId },
-//         { $set: req.body },
-//         { runValidators: true, new: true }
-//       );
-
-//       if (!course) {
-//         res.status(404).json({ message: 'No course with this id!' });
-//       }
-
-//       res.json(course);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   },
-};
